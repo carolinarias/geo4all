f6995e0b51659d5266e1446ca3aa4989eca7fa6c
Vaclav Petras <wenzeslaus@gmail.com>
2016-06-06 10:08:11 -0400

    add html title

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 ./copy-common-files.py --dst-dir=build
 
 ./build-slides.py --outdir=build \
+    --title "Geo4all - OSGeo education and research outreach: how it works and how to join" \
     index.html
